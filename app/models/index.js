64932b190cc966d301387dd39d1aaedd6351b7a8
prakash_28 <prakashraaj7996@gmail.com>
2023-11-27 14:09:13 -0500

    chore: product, wishlist, reviews

--- app/models/index.js
+++ app/models/index.js
@@ -2,13 +2,23 @@ import mongoose from "mongoose";
 import Role from "./role.model.js";
 import Tutorial from "./tutorial.model.js";
 import User from "./user.model.js";
+import Product from "./product.model.js";
+import Address from "./address.model.js";
+import Cart from "./cart.model.js";
+import Wishlist from "./wishlist.model.js";
+import Review from "./review.model.js";
 
 mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
-// db.tutorial = Tutorial;
+db.tutorial = Tutorial;
 db.user = User;
 db.role = Role;
+db.product = Product;
+db.address = Address;
+db.review = Review;
+db.wishlist = Wishlist;
+db.cart = Cart;
 
 db.ROLES = ["user", "admin", "moderator"];
 
